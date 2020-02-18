fee807b8cecc10dc91f9ea3c0c3d4e34eb968b38
sergio2294 <50100113+sergio2294@users.noreply.github.com>
2020-02-18 12:09:29 +0100

    Módulo de Odoo
    Practica final

---
+++ dev_manager1/__openerp__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Developments Manager',
+    'version': '1.1',
+    'author': 'Sergio de Oro',
+    'description': 'Modulo de gestion de solicitudes de desarrollo',
+    'depends': ['base'],
+    'application': True,
+    'data' : ['views/devman_views.xml'],
+}
